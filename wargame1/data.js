01473a0d8baf515f9e7430c0ad7c0e4be8badfe7
Michal Louda <mloud@seznam.cz>
2026-09-01 17:46:12 +0200

    texts

--- wargame1/data.js
+++ wargame1/data.js
@@ -8,13 +8,13 @@ const wargame1Data = [
     {
         "id": 1,
         "title": "Level 1",
-        "description": "Nyní jsi přihlášen jako Level1. Heslo pro Level2 je ve složce Neporadek na tvé ploše. Je tam ale obrovské množství zbytečných dat. Hledaný soubor s heslem není na první pohled vidět a navíc se brání úpravám – je v systému označen pouze pro čtení. Budeš muset použít příkazovou řádku (CMD), abys ho odhalil a přečetl.",
+        "description": "Nyní jsi přihlášen jako Level1. Heslo pro Level2 je ve složce Neporadek na tvé ploše. Je tam ale obrovské množství zbytečných dat. Hledaný soubor s heslem není na první pohled vidět a navíc se brání úpravám – je v systému označen pouze pro čtení. Musíš použít JEN příkazovou řádku (CMD), abys ho odhalil a přečetl.",
         "help": "dir, attrib, type"
     },
     {
         "id": 2,
         "title": "Level 2",
-        "description": "Úspěšně ses dostal do účtu Level2. Heslo k další úrovni je bezpečně uloženo v C:\\Trezor\\klic. Když se ho ale pokusíš otevřít, systém tě odmítne kvůli chybějícím oprávněním. Máš ale práva administrátora! Zjisti, jak nad souborem násilně převzít vlastnictví a jak si následně přidělit práva ke čtení, abys soubor otevřel.",
+        "description": "Úspěšně ses dostal do účtu Level2. Heslo k další úrovni je bezpečně uloženo v C:\\Trezor. Když se ho ale pokusíš otevřít, systém tě odmítne kvůli chybějícím oprávněním. Máš ale práva administrátora! Zjisti, jak nad souborem násilně převzít vlastnictví a jak si následně přidělit práva ke čtení, abys soubor otevřel.",
         "help": "Karta zabezpečení souboru"
     },
     {
@@ -26,7 +26,7 @@ const wargame1Data = [
     {
         "id": 4,
         "title": "Level 4",
-        "description": "Heslo se nachází na fyzickém disku v tomto počítači. Systém ale tento diskový oddíl (partition) momentálně \"nevidí\" a Průzkumník souborů ti nijak nepomůže. Musíš vstoupit do administrátorské konzole a pomocí příkazové řádky zjistit, jaké oddíly se na disku nacházejí. Najdi svazek formátovaný jako FAT32, kterému chybí písmeno, a ručně mu nějaké přiřaď, abys z něj mohl číst.",
+        "description": "Heslo se nachází na fyzickém disku v tomto počítači. Systém ale tento diskový oddíl (partition) momentálně \"nevidí\" a Průzkumník souborů ti nijak nepomůže. Musíš spustit Správu disků (diskmgmt.msc) jako administrátor (heslo admin). Najdi skrytý svazek a zviditelni ho",
         "help": "Není třeba nápověda"
     },
     {
